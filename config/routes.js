47ac7a6ca5db3db0825d99fea0a55885115f2cf4
Ma5ee2 <hussainmasee@gmail.com>
2017-12-06 14:12:59 +0000

    added restful routes

--- config/routes.js
+++ config/routes.js
@@ -1,2 +1,15 @@
 const router = require('express').Router();
 const foodbanks  = require('../controllers/foodbanks');
+
+router.route('/foodbanks')
+  .get(foodbanks.index)
+  .post(foodbanks.create);
+
+router.route('/foodbanks/:id')
+  .get(foodbanks.show)
+  .put(foodbanks.update)
+  .delete(foodbanks.delete);
+
+router.all('/*', (req, res) => res.notFound());
+
+module.exports = router;
