60e00a82d1386523ad7952278a8d2351920b59d6
Ma5ee2 <hussainmasee@gmail.com>
2017-12-07 14:59:53 +0000

    index page displays images and image has a link for the show page

--- src/app.js
+++ src/app.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+
+import FoodbanksIndex from './components/foodbanks/FoodbanksIndex';
+
+import 'bootstrap-css-only';
+import './scss/style.scss';
 
 class App extends React.Component {
 
   render() {
     return (
-      <h1>WDI Project 4: MERN Stack App</h1>
+      <Router>
+        <div className="container">
+          <header>
+            <h1>FoodBank Donator</h1>
+            <h2>Help a foodbank near you.</h2>
+            <hr/>
+          </header>
+          <main>
+            <Route exact path="/" component={FoodbanksIndex} />
+          </main>
+        </div>
+      </Router>
     );
   }
 }
 
 ReactDOM.render(
   <App />,
-  document.getElementById('app')
+  document.getElementById('root')
 );
---
+++ src/components/foodbanks/FoodbanksIndex.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class FoodbanksIndex extends React.Component {
+  state = {
+    foodbanks: []
+  }
+
+  componentDidMount() {
+    Axios
+    .get('/api/foodbanks')
+    .then(res => this.setState({ foodbanks: res.data }))
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="page-banner col-md-12">
+            <div>
+              {this.state.foodbanks.map(foodbank => {
+                return (
+                  <div key={foodbank.id} className="image-tile col-md-4 col-sm-6 col-xs-12">
+                    <Link to={`/foodbanks/${foodbank.id}`}>
+                    <img src={foodbank.image} className="img-responsive" />
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+}
+
+export default FoodbanksIndex;
